06e1d425a5ea3d0b91d892bb621055411a6b0fc8
Molowayi <molowayi@gmail.com>
2023-12-25 16:57:36 +0100

    Project file 2017-2020 refoctored

--- data_science_2023/projet_2017_2020.py
+++ data_science_2023/projet_2017_2020.py
@@ -11,16 +11,43 @@ import numpy as np
 
 def je_fais_un_histogramme(df, nom_colonne):
     (df[nom_colonne]).hist()
+def un_histogramme(df, nom_colonne, titre, x_label, y_label):
+    vc = df[nom_colonne].value_counts()
+    print("vc : \n", vc)
+    print("vc index : \n", vc.index)
 
-def je_fais_un_donut(nom_colonne = 'Montant total'):
-    folder = Path(__file__).parent
-    df = pd.read_csv(folder / '2017_to_2020_sacgas.csv', sep=';')
-    df_montants = df[nom_colonne].value_counts()
+    grand_font = {
+        'family': 'serif',
+        'color':  'darkred',
+        'weight': 'bold',
+        'size': 12,
+    }   # https://matplotlib.org/stable/gallery/text_labels_and_annotations/text_fontdict.html
+    font = {
+        'family': 'serif',
+        'color':  'darkred',
+        'weight': 'bold',
+        'size': 10,
+    }
+    petit_font = {
+        'family': 'serif',
+        'color':  'darkred',
+        'weight': 'bold',
+        'size': 8,
+    }    
+    plt.xlabel(x_label, fontdict=petit_font)
+    plt.ylabel(y_label, fontdict=petit_font)
+    plt.title(f"Effectifs des {titre}", fontdict=grand_font)
+    plt.bar(vc.index, vc)
+    # plt.xticks(rotation = 90)
+    return plt
+def je_fais_un_donut(titre, dataframe, nom_colonne):
+    
+    df_colonne = dataframe[nom_colonne].value_counts()
     fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
 
-    recipe = df_montants.index 
+    recipe = df_colonne.index 
 
-    data = df_montants  #[225, 90, 50, 60, 100, 5]
+    data = df_colonne  #[225, 90, 50, 60, 100, 5]
 
     wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
 
@@ -38,9 +65,10 @@ def je_fais_un_donut(nom_colonne = 'Montant total'):
         ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
                     horizontalalignment=horizontalalignment, **kw)
 
-    ax.set_title("Diagramme des montants des infractcions")
+    ax.set_title(titre)
 
-    return mpl_to_plotly(fig)
+    # return mpl_to_plotly(fig)
+    return plt
 
 def je_fais_un_diagramme_circulaire(nom_colonne):
     pass
@@ -61,12 +89,12 @@ if __name__ == '__main__':
 
  #   print(df["Montant total"])
 
-    #%% Voir combien de catégories de sanction il y a
+#%% Voir combien de catégories de sanction il y a
     setCategories = set()
     for cat in df["Catégorie"]:
         setCategories.add(cat)
 #    print(setCategories)
-    # %% Voir les différentes infractions et montants
+# %% Voir les différentes infractions et montants
     infractions = set()
     for cat in df["Infractions"]:
         infractions.add(cat)
@@ -82,11 +110,11 @@ if __name__ == '__main__':
     s = pd.Series(list_montants)
     # print(s.describe())
     s.plot.hist()
-    plt.ylabel("Fréquence")
-    plt.xlabel("Montant contravention")
-    plt.title("Année 2018")
-    plt.hist(s)
-    s.hist()
+    # plt.ylabel("Fréquence")
+    # plt.xlabel("Montant contravention")
+    # plt.title("Année 2018")
+    # plt.hist(s)
+    # s.hist()
     df["Règlement"].hist()
     df["Catégorie"].hist()
     # plt.hist(df["Catégorie"])
@@ -104,61 +132,22 @@ if __name__ == '__main__':
     # print(df_colonnes_pour_histogramme)
     #  df_colonnes_pour_histogramme["Montant total"].hist()
     # %% Histogramme des règlements
-    vc = df["Règlement"].value_counts()
-    # print("vc : \n", vc)
-    # print("vc index : \n", vc.index)
-
-    font = {'family': 'serif',
-            'color':  'darkred',
-            'weight': 'bold',
-            'size': 12,
-            }   # https://matplotlib.org/stable/gallery/text_labels_and_annotations/text_fontdict.html
-
-    plt.xlabel("Règlement enfreint", fontdict=font)
-    plt.ylabel("Fréquences")
-    plt.title("Fréquences des infractions aux différents règlements", fontdict=font)
-    plt.bar(vc.index, vc)
-    plt.bar(["AUDERGHEM-RGP", "REGLEMENT GENERAL DE POLICE COMMUN AUX 19 COMMUNES BRUXELLOISES", "CODE DE LA ROUTE", "CODE PENAL"], vc)
-    plt.xticks(rotation = 40)
-    plt.show()
+    un_histogramme(df,"Règlement","Réglements", "Réglement", "Effectif")
+    # %% Histogramme pour quelques colonnes
+    un_histogramme(df,"Langue","Langues", "Langue", "Effectif")
+    # %% Histogramme pour quelques colonnes
+    un_histogramme(df,"Année faits","années", "Année", "Effectif")
+    # %% Histogramme pour quelques colonnes
+    un_histogramme(df,"Montant total","Montant totaux", "Montant total", "Effectif")
+    # %% Histogramme pour quelques colonnes
+    un_histogramme(df,"Catégorie","Catégories", "Catégorie", "Effectif")
+    # je_fais_un_histogramme("Règlement")
     # %% Diagramme circulaire des  montants : https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html#sphx-glr-gallery-pie-and-polar-charts-pie-and-donut-labels-py
     df_montants = df["Montant total"].value_counts()
     # print(df_montants)
     # print(df_montants.index)
-    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-
-    recipe = df_montants.index #["225 g flour",
-            #   "90 g sugar",
-            #   "1 egg",
-            #   "60 g butter",
-            #   "100 ml milk",
-            #   "1/2 package of yeast"]
-
-    data = df_montants  #[225, 90, 50, 60, 100, 5]
-
-    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
-
-    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
-    kw = dict(arrowprops=dict(arrowstyle="-"),
-            bbox=bbox_props, zorder=0, va="center")
-
-    for i, p in enumerate(wedges):
-        ang = (p.theta2 - p.theta1)/2. + p.theta1
-        y = np.sin(np.deg2rad(ang))
-        x = np.cos(np.deg2rad(ang))
-        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-        connectionstyle = f"angle,angleA=0,angleB={ang}"
-        kw["arrowprops"].update({"connectionstyle": connectionstyle})
-        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
-                    horizontalalignment=horizontalalignment, **kw)
-
-    ax.set_title("Diagramme des montants des infractions")
-
-    plt.show()
-
     # Carte Auderghem : https://www.google.com/maps/place/Auderghem/@50.8098864,4.4421555,14z/data=!4m6!3m5!1s0x47c3a7be0c453d4b:0x143c8e127699e0ed!8m2!3d50.8165284!4d4.4263428!16zL20vMDZuejZf?entry=ttu
-
     # %% Les fonctions qui créent les graphiques pour Dash (voir plus haut)
-    je_fais_un_donut()
+    je_fais_un_donut("Montants sanctions", df, "Montant total").show()
 
 # %%
